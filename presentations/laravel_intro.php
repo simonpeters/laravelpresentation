85e33390cbbbc2417e2d70af6cea3168c7909084
Simon Peters <simon@contentcowboys.com>
2013-12-07 13:21:00 +0100

    intro done

--- presentations/laravel_intro.php
+++ presentations/laravel_intro.php
@@ -1,6 +1,28 @@
 <section>
 	<h1>Laravel</h1>
-	<h2>By Vincent Peters</h2>
-	<h2>And Simon Peters</h2>
+	<h2>Door Vincent Peters</h2>
+	<h2>En Simon Peters</h2>
 	<h4>A.K.A The Fire Brothers</h4>
+</section>
+<section>
+	<h1>Wat is Laravel</h1>
+	<blockquote>
+		&ldquo;Laravel is a web application framework with expressive, elegant syntax. Laravel attempts to take the pain out of development by easing common tasks used in the majority of web projects, such as authentication, routing, sessions, and caching.&rdquo;
+	</blockquote>
+</section>
+<section>
+	<h1>Hoe ontstaan</h1>
+	<ul>
+		<li>2009 ( Samen met PHP 5.3 )</li>
+		<li>Taylor Otwell</li>
+	</ul>
+</section>
+<section>
+	<h1>Waarom Laravel (4)</h1>
+	<ul>
+		<li>Expressief</li>
+		<li>Grote community</li>
+		<li>Gebaseerd op Composer en Symphony2</li>
+		<li>Fun to use</li>
+	</ul>
 </section>
---
+++ presentations/routing.php
@@ -0,0 +1,3 @@
+<section>
+	<h1>Laravel routing</h1>
+</section>
